83b3072d114c7dfcb36c7d6196fd4d75ed4ec8cc
antoinethebault <antoine.thebau@gmail.com>
2020-03-04 14:04:43 +0100

    tp2 heritage redefinition toString 2

--- src/fr/diginamic/banque/entites/Compte.java
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -6,12 +6,16 @@ package fr.diginamic.banque.entites;
  * et un constructeur permettant de creer une instance
  */
 public class Compte {
+	/**
+	 * numero 	numero de compte
+	 * solde 	solde du compte
+	 */
 	private String numero;
 	private double solde;
 	
 	/**
-	 * @param numeroCompte
-	 * @param soldeCompte
+	 * @param numeroCompte	numero du compte
+	 * @param soldeCompte	solde du compte
 	 */
 	public Compte(String numero, double solde) {
 		this.numero = numero;
@@ -21,6 +25,7 @@ public class Compte {
 	/**
 	 *redefinition de la methode toString afin d'afficher le numero de compte et le solde
 	 */
+	@Override
 	public String toString () {
 		return "Numero de compte : "+numero+" Solde : "+solde;
 	}
